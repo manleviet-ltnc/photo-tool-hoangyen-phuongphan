c0da70131242fb2a3e02c1de4ec8778494b74419
phanphuong1020 <phanthiphuong1020@gmail.com>
2017-10-09 18:41:52 +0700

    2.3.2

---
+++ photo tool/photo tool/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace photo_tool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            SetTitleBar();
+        }
+        private void SetTitleBar()
+        {
+            Version ver = new Version(Application.ProductVersion);
+            Text = String.Format("Myphotos {0:0}.{1:0}",
+                                 ver.Major, ver.Minor);
+
+        }
+    }
+}
