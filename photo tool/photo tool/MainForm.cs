2d0394a58e8b5952f533c9bfef997a0d17332616
phanphuong1020 <phanthiphuong1020@gmail.com>
2017-10-11 02:18:36 +0700

    A

--- photo tool/photo tool/MainForm.cs
+++ photo tool/photo tool/MainForm.cs
@@ -16,6 +16,7 @@ namespace photo_tool
         {
             InitializeComponent();
             SetTitleBar();
+            SetStatusStrip(null);
         }
         private void SetTitleBar()
         {
@@ -24,5 +25,82 @@ namespace photo_tool
                                  ver.Major, ver.Minor);
 
         }
+
+        private void mnuFileLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open Photo";
+            dlg.Filter = "jpg files (*.jpg)|*.jpg"
+                          + "|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pbxPhoto.Image = new Bitmap(dlg.OpenFile());
+                }
+                catch (ArgumentException ex)
+                {
+                    MessageBox.Show("Unable to load file:" + ex.Message);
+                    pbxPhoto.Image = null;
+                }
+                SetStatusStrip(dlg.FileName);
+            } 
+            dlg.Dispose();
+        }
+
+        private void mnuFileExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void mnuImage_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            processImageClick(e);
+        }
+
+        private void processImageClick(ToolStripItemClickedEventArgs e)
+        {
+            ToolStripItem item = e.ClickedItem;
+            string enumVal = item.Tag as string;
+            if (enumVal != null)
+            {
+                pbxPhoto.SizeMode = (PictureBoxSizeMode)Enum.Parse(typeof(PictureBoxSizeMode), enumVal);
+            }
+        }
+
+        private void mnuImage_DropDownOpening(object sender, EventArgs e)
+        {
+            processImageOpening(sender as ToolStripDropDownItem);
+        }
+        private void processImageOpening(ToolStripDropDownItem parent)
+        {
+            if (parent != null)
+            {
+                string enumVal = pbxPhoto.SizeMode.ToString();
+                foreach (ToolStripMenuItem item in parent.DropDownItems)
+                {
+                    item.Enabled = (pbxPhoto.Image != null);
+                    item.Checked = item.Tag.Equals(enumVal);
+                }
+            }
+        }
+        private void SetStatusStrip(string path)
+        {
+            if (pbxPhoto.Image != null)
+            {
+                sttInfo.Text = path;
+                sttImageSize.Text = string.Format("{0:#}*{1:#}",
+                                                   pbxPhoto.Image.Width,
+                                                   pbxPhoto.Image.Height);
+                //sttAlbumPos in set in ch. 6
+            }
+            else
+            {
+                sttInfo.Text = null;
+                sttImageSize.Text = null;
+                sttAlbumPos.Text = null;
+            }
+        }
     }
+
 }
